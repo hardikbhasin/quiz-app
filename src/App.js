1b5585a1f5938a4aaaa17c6f891282d98c1d88ea
hardikbhasin <hardikbhasin398@gmail.com>
2023-04-11 22:40:43 +0530

    added bg image

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Hello
+    <div className="App"
+    style={{backgroundImage:"url(./ques1.png)"}}>
+      
     </div>
   );
 }
