a19862725e108277250f95a1d1f7d870dca8ed5b
hardikbhasin <hardikbhasin398@gmail.com>
2023-04-12 23:56:36 +0530

    added text fields ( not categories yet)

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { MenuItem, TextField } from '@material-ui/core'
+import "./Home.css"
 
 const Home = () => {
     return (
-        <div>
-             Homepage
+        <div className='content'>
+            <div className='settings'>
+                <span style={{ fontSize: 30 }}>Quiz settings</span>
+                <div className='settings_select'>
+                    <TextField style={{marginBottom:25}}label='Enter your name' variant='outlined'></TextField>
+                    <TextField select label='Select category' variant='outlined' style={{marginBottom:30}}>
+                        
+                        <MenuItem>
+                        </MenuItem>
+                    </TextField>
+                    
+
+                </div>
+            </div>
+            <img src="/quiz.svg" className='banner' alt="quiz-image"></img>
         </div>
     )
 }
