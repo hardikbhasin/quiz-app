e24278393b1c3207202739583f5d161fa679649e
hardikbhasin <hardikbhasin398@gmail.com>
2023-04-12 18:58:27 +0530

    Header initilize

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
-
+import Header from "./components/Header/Header";
 function App() {
   return (
     <div className="app"
     style={{backgroundImage:"url(./ques1.png)"}}>
+      <Header />
       
     </div>
   );
---
+++ src/components/Header/header.js
@@ -0,0 +1,13 @@
+import {Link} from "react-router-dom";
+import "./Header.css";
+import React from 'react'
+
+const   Header = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Header
