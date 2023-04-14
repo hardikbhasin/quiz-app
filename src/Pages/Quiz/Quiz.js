22fbef2544785624609244d11e194d2ba4ace084
hardikbhasin <hardikbhasin398@gmail.com>
2023-04-14 19:03:44 +0530

    shuffled options from API

--- src/Pages/Quiz/Quiz.js
+++ src/Pages/Quiz/Quiz.js
@@ -1,10 +1,25 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
-const Quiz = ({name,score,questions,setQuestions,setScore}) => {
+const Quiz = ({ name, score, questions, setQuestions, setScore }) => {
+  const [options, setOptions] = useState()
+  const [currQues, setCurrQues] = useState(0)
+
+
+
+
   useEffect(() => {
     console.log(questions);
+    setOptions(questions && handelShuffle([
+      questions[currQues]?.correct_answer,
+      ...questions[currQues]?.incorrect_answers
+    ])
+    );
 
-  }, [questions])
+  }, [questions]);
+   console.log(options);
+  const handelShuffle=(optionss) =>{
+    return optionss.sort(()=>Math.random()-0.5);
+  }
 
 
 
