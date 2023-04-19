a1deb8cdbad8dcdc945ab55b40733f3584533de8
hardikbhasin <hardikbhasin398@gmail.com>
2023-04-19 19:11:51 +0530

    added single question and it's options

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
 
 const Question = ({ currQues,
   setCurrQues,
@@ -17,9 +18,26 @@ const Question = ({ currQues,
   return (
     <div>
       <h1>Question {currQues + 1}</h1>
+      <div className='singleQuestion'>
+        <h2>{questions[currQues].question}</h2>
+        <div className='options'>
+        {error && <ErrorMessage>{error}</ErrorMessage>}
+        {
+          options && 
+          options.map(i =>(
+            <button>{i}</button>
+
+          ))
+        }
+
+        </div>
+
+
+      </div>
 
 
     </div>
+
   )
 }
 
